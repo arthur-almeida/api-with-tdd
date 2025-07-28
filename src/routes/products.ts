eb5f9a868b970d3892cf399cc82718f4050a8197
Arthur Almeida <arthurdosantosalmeida@gmail.com>
2025-07-28 16:48:28 +0000

    refactor: create controller and return a list of products from the model

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import { ProductsController } from "../controllers/products.ts";
+import { Product } from "../models/product.ts";
 
 export const productsRouter = Router();
-const productsController = new ProductsController();
+const productsController = new ProductsController(Product);
 
 productsRouter.get("/", (req, res) => productsController.get(req, res));
--- tests/integration/routes/products.test.ts
+++ tests/integration/routes/products.test.ts
@@ -1,6 +1,8 @@
 import type { Express } from "express";
+import mongoose from "mongoose";
 import request from "supertest";
 import setupApp from "../../../src/app.ts";
+import { Product } from "../../../src/models/product.ts";
 
 describe("Routes: Products", () => {
   let app: Express;
@@ -9,10 +11,26 @@ describe("Routes: Products", () => {
     description: "Product description",
     price: 100,
   };
+  const productId = new mongoose.Types.ObjectId("56cb91bdc3464f14678934ca");
+  const expectedProduct = {
+    __v: 0,
+    _id: productId.toString(),
+    ...defaultProduct,
+  };
 
   beforeAll(async () => {
     app = await setupApp();
   });
+
+  beforeEach(async () => {
+    await Product.deleteMany();
+
+    const product = new Product(defaultProduct);
+    product._id = productId;
+    return await product.save();
+  });
+
+  afterEach(async () => await Product.deleteMany());
 
   afterAll(async () => {
     await app.database?.close();
@@ -21,7 +39,7 @@ describe("Routes: Products", () => {
   describe("GET /products", () => {
     it("should return a list of products", async () => {
       const response = await request(app).get("/products").expect(200);
-      expect(response.body[0]).toStrictEqual(defaultProduct);
+      expect(response.body).toStrictEqual([expectedProduct]);
     });
   });
 });
